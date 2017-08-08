1205341786d34c3ad919a7c6decb801f93ac4740
Samuel <samuel.lopez@sussexcampervans.com>
2017-08-08 22:01:35 +0100

    Python Email Project
    Pushing python file.

---
+++ send_email.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+email_user = 'your_email'
+email_password = 'your_password'
+email_send = 'recipient_email'
+
+subject = 'subject'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi there, sending this email from Python!'
+msg.attach(MIMEText(body,'plain'))
+
+filename='filename'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
